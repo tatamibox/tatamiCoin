59988f400e8911bb09d3a0836fb63c360e48bd0c
tatamibox <tat4mi@gmail.com>
2022-06-22 22:19:32 -0700

    finish navbar

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/views'))
+app.use(express.static(path.join(__dirname, 'public')))
+app.listen(3000);
+
+app.get('/', (req, res) => {
+    res.render('index.ejs');
+
+})
+
