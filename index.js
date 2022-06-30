866a903f27d91d74adb8008e9658570eb0151b5a
tatamibox <tat4mi@gmail.com>
2022-06-30 10:11:39 -0700

    add POST route for new proposals

--- index.js
+++ index.js
@@ -8,10 +8,11 @@ const Project = require('./models/project');
 const catchAsync = require('./utilities/catchAsync');
 //
 
-// expres config
+// express config
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'))
 app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.urlencoded({ extended: true }));
 app.listen(3000);
 
 //
@@ -27,30 +28,41 @@ mongoose.connect('mongodb://127.0.0.1:27017/accelerator', { useNewUrlParser: tru
     })
 
 
-// static webpages
+// static webpages -----v
 
 
-
+// index page
 app.get('/', (req, res) => {
     res.render('index');
 
 })
 
+// featured projects / accelerator projects page
 app.get('/accelerator', async (req, res) => {
     const p = await Project.find({});
     res.render('accelerator', { p })
 })
 
-
+// CREATE section, submit new proposal
 app.get('/accelerator/new', (req, res) => {
     res.render('new');
 })
 
+// SHOW/READ section, show a project by ID
 app.get('/accelerator/:id', catchAsync(async (req, res) => {
     const { id } = req.params;
     const p = await Project.findById(id);
     res.render('show', { p })
 }))
+
+// POST route for new projects being submitted
+app.post('/accelerator', catchAsync(async (req, res) => {
+    console.log(req.body)
+    const project = new Project(req.body);
+    await project.save();
+    res.redirect(`/accelerator/${project._id}`)
+}))
+
 
 //error handling
 app.all('*', (req, res, next) => {
